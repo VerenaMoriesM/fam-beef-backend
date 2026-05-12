3a3366709cdc4b4b49fb91a5ca93cf3b8a117c00
VerenaMoriesM <verena.mories@mitchdesigns.com>
2026-05-12 17:44:09 +0300

    fix railway postgres config

--- config/database.ts
+++ config/database.ts
@@ -61,14 +61,35 @@
 // };
 
 // export default config;
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false,
+import path from 'path';
+
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
+      pool: { min: 0, max: 10 },
     },
-  },
-});
+
+    sqlite: {
+      connection: {
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: 60000,
+    },
+  };
+};
--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,18 @@
-import type { Core } from '@strapi/strapi';
+// import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-  host: env('HOST', '0.0.0.0'),
+// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
+// export default config;
+export default ({ env }) => ({
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
-
-export default config;
